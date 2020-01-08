29840f3b2baaab10a9bfc7bd794fa5392da9caf1
waspn <proto_v6@hotmail.com>
2020-01-08 14:53:56 +0700

    create info page

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react'
 const { ipcRenderer } = window.require('electron')
 
 class Info extends Component {
-
-  componentDidMount() {
-    
+  constructor (props) {
+    super(props)
+    this.state = {
+      detail: []
+    }
   }
 
-  render() {
-    return(
+  componentDidMount () {
+    ipcRenderer.on('faq-detail', (event, detail) => {
+      this.setState({ detail })
+    })
+  }
+
+  render () {
+    console.log('DETAIL sTESAETAS', this.state.detail)
+    const { detail } = this.state
+    return (
       <div>
-        INFOINFOFINFINFIOF
+        <h3>{detail.question}</h3>
+        <hr />
+        {detail.answer && detail.answer.map(ans => <p>{ans}</p>)}
       </div>
     )
   }

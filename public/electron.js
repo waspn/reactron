bc29c5465b44ab8afd4599bf07c394e155264fc7
waspn <proto_v6@hotmail.com>
2020-01-08 14:55:00 +0700

    handle open new window

--- public/electron.js
+++ public/electron.js
@@ -1,25 +1,55 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
 
-let mainWindow;
-function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+let mainWindow
+let infoWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: { nodeIntegration: true }
+  })
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
-  );
-  mainWindow.on('closed', () => (mainWindow = null));
+  )
+  mainWindow.on('closed', () => (mainWindow = null))
+
+  infoWindow = new BrowserWindow({
+    width: 900,
+    height: 500,
+    parent: mainWindow,
+    show: false,
+    webPreferences: { nodeIntegration: true }
+  })
+  infoWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000/info'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  infoWindow.on('closed', (event) => {
+    event.preventDefault()
+    infoWindow.hide()
+  })
 }
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
+
+ipcMain.on('show-more-info', (event, info) => {
+  // console.log('info', info)
+  // console.log('get-show-more-info')
+  infoWindow.show()
+  infoWindow.webContents.send('faq-detail', info)
+})

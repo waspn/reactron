fa5d0611889da1e54a536cbf0a50041b45b9608b
waspn <proto_v6@hotmail.com>
2020-01-08 14:54:12 +0700

    fix faqPanel

--- src/Components/FaqPanel.js
+++ src/Components/FaqPanel.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
-import { ipcRenderer } from 'electron'
+const { ipcRenderer } = window.require('electron')
 
 class FaqPanel extends Component {
-
-  openInfo = (info) => {
+  openInfo = info => {
     ipcRenderer.send('show-more-info', info)
   }
 
-  render() {
+  render () {
     const { faqData } = this.props
     return faqData.map((info, key) => {
       return (
         <div>
-          <button key={key} onClick={() => { this.openInfo(info) }}>
-            <pre style={{ margin: 5, fontSize: 20, fontFamily: 'Helvatica' }}>{info.question}</pre>
+          <button key={key} onClick={() => {this.openInfo(info)}}>
+            <pre style={{ margin: 5, fontSize: 20, fontFamily: 'Helvatica' }}>
+              {info.question}
+            </pre>
           </button>
         </div>
       )

34d602a7d60099a2f7346c68bfb45716cfd2b3a9
waspn <proto_v6@hotmail.com>
2020-01-08 14:55:39 +0700

    receive mock data (change later)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import FaqPanel from './Components/FaqPanel'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const faqData = {
+  data: [
+    {
+      question: 'egrevsffefsefesf',
+      answer: ['awwafegrereiul', 'yttyj65trtrt', 'ewgrebb wse']
+    },
+    {
+      question: '7ikuruywe',
+      answer: ['1204;ihoyrdrt', 'ewgrebb wse']
+    },
+    {
+      question: '45mwszeszdasww',
+      answer: ['po;,uyt5sre', '87yrxfxnjfr wse']
+    }
+  ]
 }
 
-export default App;
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            FAQ - <code>Frequently Ask Question</code>
+          </p>
+          <FaqPanel faqData={faqData.data} />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App

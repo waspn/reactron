431d9b88a9fd8856275734529fc7894ae9f7e7a8
waspn <proto_v6@hotmail.com>
2020-01-08 14:27:07 +0700

    add router

---
+++ src/Components/Info.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+const { ipcRenderer } = window.require('electron')
+
+class Info extends Component {
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return(
+      <div>
+        INFOINFOFINFINFIOF
+      </div>
+    )
+  }
+}
+
+export default Info
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,12 +2,13 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import App from "../App";
+import Info from '../Components/Info'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      {/* <Route exact path="/info" component={Info} /> */}
+      <Route exact path="/info" component={Info} />
     </Switch>
   </BrowserRouter>
 );
